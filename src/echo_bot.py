20bd471956fa063250a96ef1f33d5c0ddfaeeea9
SUNNY BHAVEEN CHANDRA <34603575+c17hawke@users.noreply.github.com>
2023-04-20 12:04:57 +0000

    echo bot created

---
+++ src/echo_bot.py
@@ -0,0 +1,36 @@
+"""
+This is a echo bot.
+It echoes any incoming text messages.
+"""
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_TOKEN = os.getenv("TOKEN")
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.answer(message.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
